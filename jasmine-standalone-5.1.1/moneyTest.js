ed381bfcf90fb2a6fa9ff4a06c6eeb8282d2ca0d
CookieDarb <darkblueauror@gmail.com>
2024-09-16 22:11:02 +0530

    added jasmine testing framework

---
+++ jasmine-standalone-5.1.1/moneyTest.js
@@ -0,0 +1,15 @@
+import { formatMoney } from "../scripts/utils/money.js";
+
+describe("test suite : formatMoney()",()=>{
+    it("normal conversion",()=>{
+        expect(formatMoney(2095)).toEqual('$20.95');
+    });
+
+    it("zero conversion",()=>{
+        expect(formatMoney(0)).toEqual("$0.00");
+    });
+
+    it("decimal conversion",()=>{
+        expect(formatMoney(2000.5)).toEqual("$20.01");
+    });
+});
